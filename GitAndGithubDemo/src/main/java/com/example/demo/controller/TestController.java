3ab8a2e74130a4d27d01fa79336c60d22bc5f4ea
AakashBawale <aakashbawale@gmail.com>
2023-10-04 18:07:15 +0530

    added policy object

--- GitAndGithubDemo/src/main/java/com/example/demo/controller/TestController.java
+++ GitAndGithubDemo/src/main/java/com/example/demo/controller/TestController.java
@@ -3,22 +3,40 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Policy;
 
 @RestController
 public class TestController {
 
-	@GetMapping("getMessege")
+	@GetMapping("/getMessege")
 	public String getMessege() {
 		String messege = "This is github practical one";
 		return messege;
 	}
 
+	@GetMapping("/employee-data")
 	public List getEmployeeData() {
 		List list = new ArrayList();
 		list.add("Ram");
 		list.add(10);
 		return list;
 	}
+
+	@GetMapping("/get-policy/{id}")
+	public ResponseEntity<Policy> getPolicyDetailById(@PathVariable int id) {
+
+		Policy policy = new Policy();
+		policy.setId(1);
+		policy.setPolicyName("Life Insurance");
+		policy.setPolicyId("p101");
+		policy.setPolicyType("life time");
+		return ResponseEntity.status(HttpStatus.OK).body(policy);
+	}
+
 }
---
+++ GitAndGithubDemo/src/main/java/com/example/demo/model/Policy.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+public class Policy {
+
+	private Integer id;
+	private String policyName;
+	private String policyId;
+	private String policyType;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPolicyName() {
+		return policyName;
+	}
+
+	public void setPolicyName(String policyName) {
+		this.policyName = policyName;
+	}
+
+	public String getPolicyId() {
+		return policyId;
+	}
+
+	public void setPolicyId(String policyId) {
+		this.policyId = policyId;
+	}
+
+	public String getPolicyType() {
+		return policyType;
+	}
+
+	public void setPolicyType(String policyType) {
+		this.policyType = policyType;
+	}
+
+}
